ea9f2d14d421f5f046ace3abce463a8e8a405255
Edwin Amaya <eamayag@unah.hn>
2021-02-13 14:50:36 -0600

    Se agrego la pagina de Productos

---
+++ ProyectoFarmacia/ProyectoFarmacia.web/Controllers/ProductosController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ProyectoFarmacia.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
