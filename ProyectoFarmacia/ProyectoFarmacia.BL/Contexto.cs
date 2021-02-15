79fd269b1ef62daf1c5edb05a6c44d13461e0c2e
Edwin Amaya <eamayag@unah.hn>
2021-02-14 22:02:23 -0600

    Se agrego Entity framework Code First

---
+++ ProyectoFarmacia/ProyectoFarmacia.BL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ProyectoFarmacia.BL
+{
+
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\ProyectoFarmacia.DB.mdf")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+    }
+}
--- ProyectoFarmacia/ProyectoFarmacia.Win/Properties/AssemblyInfo.cs
+++ ProyectoFarmacia/ProyectoFarmacia.Win/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18f99368-fe8d-4b2d-a1fe-5668fb688ae7")]
+[assembly: Guid("3a0b3370-0929-42bf-9c77-2a1e52b75572")]
 
 // Version information for an assembly consists of the following four values:
 //
