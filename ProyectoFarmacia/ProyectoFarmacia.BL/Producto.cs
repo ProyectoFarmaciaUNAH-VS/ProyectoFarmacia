88211f84a99cd871abd71895743e4fca3ef1b8b1
Ham <Ham@DESKTOP-77CINMK>
2021-02-20 03:29:19 -0600

    agregar, detalles

--- ProyectoFarmacia/ProyectoFarmacia.BL/Producto.cs
+++ ProyectoFarmacia/ProyectoFarmacia.BL/Producto.cs
@@ -8,9 +8,10 @@ namespace ProyectoFarmacia.BL
 {
     public class Producto
     {
+        internal int Id;
+
         public int ID { get; set; }
         public string Descripcion { get; set; }
         public double Precio { get; set; }
-
     }
 }
--- ProyectoFarmacia/ProyectoFarmacia.BL/ProductosBL.cs
+++ ProyectoFarmacia/ProyectoFarmacia.BL/ProductosBL.cs
@@ -22,6 +22,34 @@ namespace ProyectoFarmacia.BL
             return ListadeProductos;
         }
 
+        public void GuardarProducto(Producto producto)
+     
+        {
+            if(producto.ID == 0)
+
+            {
+                _contexto.Productos.Add(producto);
+              
+            }
+            else
+            {
+                var productoExistente = _contexto.Productos.Find(producto.ID);
+                productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.Precio = producto.Precio;
+
+            }
+            _contexto.SaveChanges();
+        }
+            public Producto ObtenerProducto(int id)
+        {
+            var producto = _contexto.Productos.Find(id);
+            return producto;
+        }
+          public void EliminarProducto(int id)
+        {
+            
+
+        }   
     }
  }
  
