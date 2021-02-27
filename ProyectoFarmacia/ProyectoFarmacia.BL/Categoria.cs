9703bc4a10a8e81be414ce04f2d8cf7ea36b7dc7
IsaacG4rc1a <isaac.g.17.98@gmail.com>
2021-02-27 01:20:11 -0600

    -Creando Validaciones para Productos y categorias -Guardado de imagenes de los productos -Imagen en el index

--- ProyectoFarmacia/ProyectoFarmacia.BL/Categoria.cs
+++ ProyectoFarmacia/ProyectoFarmacia.BL/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace ProyectoFarmacia.BL
     public class Categoria
     {
         public int Id { get; set; }
-        public string Descripcion { get; set; }
+		[Required(ErrorMessage = "Ingrese una categoría")]
+		[MinLength(3, ErrorMessage = "Ingrese un mínimo 3 caracteres")]
+		[MaxLength(20, ErrorMessage = "Ingrese un maximo 20 caracteres")]
+		public string Descripcion { get; set; }
     }
 }
