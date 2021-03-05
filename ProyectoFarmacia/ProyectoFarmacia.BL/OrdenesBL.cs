9a0ad7f1bcc8c6371a45b0a23d4cbe7f61b347d8
Edwin Amaya <eamayag@unah.hn>
2021-03-04 20:28:05 -0600

    Se creo Controlador, vistas y BL de: Clientes y Ordenes

---
+++ ProyectoFarmacia/ProyectoFarmacia.BL/OrdenesBL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProyectoFarmacia.BL
+{
+    public class OrdenesBL
+    {
+        Contexto _contexto;
+        public List<Orden> ListadeOrdenes { get; set; }
+
+        public OrdenesBL()
+        {
+            _contexto = new Contexto();
+            ListadeOrdenes = new List<Orden>();
+        }
+
+        public List<Orden> ObtenerOrdenes()
+        {
+            ListadeOrdenes = _contexto.Ordenes
+                .Include("Cliente")
+                .ToList();
+
+            return ListadeOrdenes;
+        }
+
+
+    }
+}
---
+++ ProyectoFarmacia/ProyectoFarmacia.WebAdmin/Controllers/OrdenesController.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoFarmacia.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFarmacia.WebAdmin.Controllers
+{
+    public class OrdenesController : Controller
+    {
+        OrdenesBL _ordenesBL;
+
+
+        public OrdenesController()
+        {
+            _ordenesBL = new OrdenesBL();
+
+        }
+
+        // GET: Ordenes
+        public ActionResult Index()
+        {
+            var listadeOrdenes = _ordenesBL.ObtenerOrdenes();
+
+            return View(listadeOrdenes);
+        }
+    }
+}
