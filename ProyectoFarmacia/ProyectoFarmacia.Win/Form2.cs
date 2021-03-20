7a2052b5ac07c52a0435e0ea29edc64f6794d99a
Ham <Ham@DESKTOP-77CINMK>
2021-03-20 00:32:56 -0600

    add form2

--- ProyectoFarmacia/ProyectoFarmacia.Win/Form2.cs
+++ ProyectoFarmacia/ProyectoFarmacia.Win/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFarmacia.BL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace ProyectoFarmacia.Win
 {
     public partial class Form2 : Form
     {
+        ReportedeVentasPorProductoBL _reporteVentasPorProductoBL;
         public Form2()
         {
             InitializeComponent();
+            _reporteVentasPorProductoBL = new ReportedeVentasPorProductoBL();
+
+            RefrescarDatos();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefrescarDatos();
+        }
+
+        private void RefrescarDatos()
+        {
+            var ListadeVentasPorProducto = _reporteVentasPorProductoBL.obtenerVentasPorProducto();
+            listadeVentasPorProductoBindingSource.DataSource = ListadeVentasPorProducto;
+            listadeVentasPorProductoBindingSource.ResetBindings(false);
         }
     }
 }
