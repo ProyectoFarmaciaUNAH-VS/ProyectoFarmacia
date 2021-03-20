7e5c57fa79480be92edb8e0029c718edeebe31a1
Ham <Ham@DESKTOP-77CINMK>
2021-03-19 19:46:01 -0600

    add admin

---
+++ ProyectoFarmacia/ProyectoFarmacia.WebAdmin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+//using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ProyectoFarmacia.WebAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //LOGIN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
